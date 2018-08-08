4094cf128b7a2477b174aa8b19c6b407d1348649
SAURABH <saurabhgupta989@gmail.com>
2018-08-08 21:08:55 +0530

    Added logger

--- mds-app/mds-service/src/main/java/com/mds/controller/UserController.java
+++ mds-app/mds-service/src/main/java/com/mds/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,6 +24,8 @@ import com.mds.service.UserService;
 @RestController
 public class UserController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private UsersMapper usersMapper;
 
@@ -30,6 +34,9 @@ public class UserController {
 
 	@PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> saveUser(@RequestBody User user) {
+
+		logger.info("saveUser invoked");
+		logger.debug("saveUser invoked");
 		Integer status = userService.saveUser(user);
 
 		if (status > 0) {
@@ -41,24 +48,34 @@ public class UserController {
 
 	@GetMapping(value = "/allUsers", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> getAll() {
+
+		logger.info("getAll invoked");
+		logger.debug("getAll invoked");
 		List<User> listOfUsers = userService.fetchAllUsers();
 		return new ResponseEntity<Object>(listOfUsers, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> getAllUsersWitDogDetails() {
+		logger.info("getAllUsersWitDogDetails invoked");
+		logger.debug("getAllUsersWitDogDetails invoked");
 		List<User> listOfUsers = userService.getAllUsersWitDogDetails();
 		return new ResponseEntity<Object>(listOfUsers, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/retrieveUser", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> retrieveUserById(@RequestParam Integer id) {
+
+		logger.info("retrieveUserById invoked");
+		logger.debug("retrieveUserById invoked");
 		User user = userService.retrieveUserById(id);
 		return new ResponseEntity<Object>(user, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> index() {
+		logger.info("index invoked");
+		logger.debug("index invoked");
 		Map<String, String> map = new HashMap<>();
 		map.put("name", "Hello World Success");
 		ResponseEntity<Object> response = null;
@@ -68,6 +85,8 @@ public class UserController {
 
 	@GetMapping("/tagJoin")
 	public List<BlogEntry> getTagJoin() {
+		logger.info("getTagJoin invoked");
+		logger.debug("getTagJoin invoked");
 		return usersMapper.findTagJoin();
 	}
 }
